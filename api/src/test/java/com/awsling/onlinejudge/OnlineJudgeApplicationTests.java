d4cdb96a1f9f54c2c8a84ae74615f8908afe12f5
awsling <awsling@outlook.com>
2024-09-20 00:13:30 +0800

    init: api init

---
+++ api/src/test/java/com/awsling/onlinejudge/OnlineJudgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awsling.onlinejudge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineJudgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
