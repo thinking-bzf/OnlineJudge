163c1f7f75aecd4728b39763eee27b3fcd1476a0
awsling <awsling@outlook.com>
2024-09-20 01:01:20 +0800

    feat: 页面布局添加

---
+++ web/src/components/GlobleHeader.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="globalHeader">
+    <div class="menu-demo">
+      <a-menu mode="horizontal" :default-selected-keys="['1']">
+        <a-menu-item
+          key="0"
+          :style="{ padding: 0, marginRight: '38px' }"
+          disabled
+        >
+          <div class="title-bar">
+            <img class="logo" src="../assets/elysia.jpg" />
+            <div class="title">SmartCode</div>
+          </div>
+        </a-menu-item>
+        <a-menu-item key="1">Home</a-menu-item>
+        <a-menu-item key="2">Solution</a-menu-item>
+        <a-menu-item key="3">Cloud Service</a-menu-item>
+        <a-menu-item key="4">Cooperation</a-menu-item>
+      </a-menu>
+    </div>
+  </div>
+</template>
+
+<script lang="ts"></script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.title-bar {
+  display: flex;
+  align-items: center;
+}
+
+.title {
+  font-size: 20px;
+  margin-left: 10px;
+}
+
+.logo {
+  height: 40px;
+}
+</style>
---
+++ web/src/layout/BasicLayout.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="height: 400px">
+      <a-layout-header class="header">
+        <GlobleHeader />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="https://github.com/thinking-bzf" target="_blank">
+          awsling github
+        </a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<style>
+#basicLayout .header {
+  background-color: #002a98;
+  color: white;
+  line-height: 64px;
+}
+
+#basicLayout .content {
+  background: linear-gradient(to right, #0f2b3d, #1b3b4b);
+  margin-bottom: 16px;
+}
+
+#basicLayout .footer {
+  background: #efefef;
+  padding: 16px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+</style>
+<script setup lang="ts">
+import GlobleHeader from "@/components/GlobleHeader.vue";
+</script>
