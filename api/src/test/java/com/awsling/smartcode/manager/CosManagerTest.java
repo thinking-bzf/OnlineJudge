d999164a5030be93783b25443f4c0a084d32bfba
awsling <awsling@outlook.com>
2024-09-24 16:32:56 +0800

    init: 后端模板初始化

---
+++ api/src/test/java/com/awsling/smartcode/manager/CosManagerTest.java
@@ -0,0 +1,23 @@
+package com.awsling.smartcode.manager;
+
+import javax.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Cos 操作测试
+ *
+ * @author <a href="https://github.com/thinking-bzf">awsling</a>
+ * 
+ */
+@SpringBootTest
+class CosManagerTest {
+
+    @Resource
+    private CosManager cosManager;
+
+    @Test
+    void putObject() {
+        cosManager.putObject("test", "test.json");
+    }
+}
