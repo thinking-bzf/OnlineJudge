463095e5ddf36f04091a0289016e5f9faab45617
awsling <awsling@outlook.com>
2024-09-24 12:57:31 +0800

    feat: 全局项目入口

--- web/src/App.vue
+++ web/src/App.vue
@@ -12,9 +12,21 @@
 import BasicLayout from "@/layout/BasicLayout.vue";
 import { useRouter } from "vue-router";
 import { useStore } from "vuex";
+import { onMounted } from "vue";
+
+// 全局初始化函数，在全局单词调用的代码，都可以写在这里
+const doInit = () => {
+  console.log("init");
+};
+
+// 在页面加载完成后调用初始化函数
+onMounted(() => {
+  doInit();
+});
 
 const router = useRouter();
 const store = useStore();
+// 判断是否有权限
 router.beforeEach((to, from, next) => {
   if (to.meta?.access === "canAdmin") {
     if (store.state.user.loginUser?.role !== "admin") {
