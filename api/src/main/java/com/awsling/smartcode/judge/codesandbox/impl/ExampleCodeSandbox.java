de051035cf8789378847f2f6b303728d4e92fcea
awsling <awsling@outlook.com>
2024-10-14 16:49:51 +0800

    feat：代码沙箱架构初始化（工厂、代理模式）

---
+++ api/src/main/java/com/awsling/smartcode/judge/codesandbox/impl/ExampleCodeSandbox.java
@@ -0,0 +1,30 @@
+package com.awsling.smartcode.judge.codesandbox.impl;
+
+import com.awsling.smartcode.judge.codesandbox.CodeSandbox;
+import com.awsling.smartcode.judge.codesandbox.model.ExecuteCodeRequest;
+import com.awsling.smartcode.judge.codesandbox.model.ExecuteCodeResponse;
+import com.awsling.smartcode.model.dto.questionsubmit.JudgeInfo;
+import com.awsling.smartcode.model.enums.QuestionSubmitStatusEnum;
+
+import java.util.List;
+
+/**
+ * 示例代码沙箱
+ */
+public class ExampleCodeSandbox implements CodeSandbox {
+
+    @Override
+    public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
+        List<String> inputList = executeCodeRequest.getInputList();
+        ExecuteCodeResponse executeCodeResponse = new ExecuteCodeResponse();
+        executeCodeResponse.setStatus(QuestionSubmitStatusEnum.SUCCEED.getValue());
+        executeCodeResponse.setMessage("代码执行成功");
+        executeCodeResponse.setOutputList(inputList);
+        JudgeInfo judgeInfo = new JudgeInfo();
+        judgeInfo.setMessage("代码执行成功");
+        judgeInfo.setTime(1000L);
+        judgeInfo.setMemory(1000L);
+        executeCodeResponse.setJudgeInfo(judgeInfo);
+        return executeCodeResponse;
+    }
+}
---
+++ api/src/main/java/com/awsling/smartcode/judge/codesandbox/model/ExecuteCodeResponse.java
@@ -0,0 +1,36 @@
+package com.awsling.smartcode.judge.codesandbox.model;
+
+import com.awsling.smartcode.model.dto.questionsubmit.JudgeInfo;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExecuteCodeResponse {
+    /**
+     * 代码执行结果
+     */
+    private List<String> outputList;
+
+    /**
+     * 接口返回的消息
+     */
+    private String message;
+
+    /**
+     * 代码执行状态
+     */
+    private Integer status;
+
+    /**
+     * 代码执行信息
+     */
+    private JudgeInfo judgeInfo;
+
+}
