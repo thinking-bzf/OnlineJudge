84eced6d01c66c2d52845cb131b7b8e46ed9943e
awsling <awsling@outlook.com>
2025-03-12 13:55:34 +0800

    feat(execute)：实现 java 原生实现

---
+++ codesanbox/src/main/java/com/awsling/codesandbox/model/ExecuteMessage.java
@@ -0,0 +1,15 @@
+package com.awsling.codesandbox.model;
+
+import lombok.Data;
+
+@Data
+public class ExecuteMessage {
+
+    private Integer exitValue;
+
+    private String message;
+
+    private String errMessage;
+
+    private Long time;
+}
---
+++ codesanbox/src/main/resources/security/DenySecurityManager.java
@@ -0,0 +1,30 @@
+public class DenySecurityManager extends SecurityManager {
+
+    // 检查所有的权限
+    // @Override
+    // public void checkPermission(Permission perm) {
+    //     throw new SecurityException("权限异常：" + perm.toString());
+    // }
+
+    @Override
+    public void checkRead(String file) {
+        throw new SecurityException("checkRead 权限异常：" + file);
+    }
+
+    @Override
+    public void checkWrite(String file) {
+        throw new SecurityException("checkWrite 权限异常：" + file);
+    }
+
+    @Override
+    public void checkExec(String cmd) {
+        throw new SecurityException("checkExec 权限异常：" + cmd);
+    }
+
+    @Override
+    public void checkConnect(String host, int port) {
+        throw new SecurityException("checkConnect 权限异常：" + host + ":" + port);
+    }
+
+
+}
