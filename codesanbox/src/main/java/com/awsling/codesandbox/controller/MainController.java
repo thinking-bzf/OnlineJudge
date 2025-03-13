e59ea5317e8bd1c3fc40944bda06cd7f5fe8fb1b
awsling <awsling@outlook.com>
2025-03-13 18:09:04 +0800

    feat(codesandbox-controller)：添加执行代码接口

--- codesanbox/src/main/java/com/awsling/codesandbox/controller/MainController.java
+++ codesanbox/src/main/java/com/awsling/codesandbox/controller/MainController.java
@@ -1,15 +1,47 @@
 package com.awsling.codesandbox.controller;
 
+import com.awsling.codesandbox.JavaDockerCodeSandbox;
+import com.awsling.codesandbox.model.ExecuteCodeRequest;
+import com.awsling.codesandbox.model.ExecuteCodeResponse;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class MainController {
 
+
+    // 鉴权请求头
+    private static final String AUTH_REQUEST_HEADER = "auth";
+    private static final String AUTH_REQUEST_SECRET = "secretKey";
+
+    @Resource
+    JavaDockerCodeSandbox javaDockerCodeSandbox;
 
     @GetMapping("/health")
     public String healthCheck() {
         return "Hello, CodeSandbox!";
     }
 
+
+    @PostMapping("/execute-code")
+    ExecuteCodeResponse executeCode(@RequestBody ExecuteCodeRequest executeCodeRequest, HttpServletRequest request
+            , HttpServletResponse response) {
+
+        String authHeader = request.getHeader(AUTH_REQUEST_HEADER);
+        if (!authHeader.equals(AUTH_REQUEST_SECRET)) {
+            response.setStatus(403);
+            return null;
+        }
+
+        if (executeCodeRequest == null) {
+            throw new RuntimeException("请求参数为空");
+        }
+        return javaDockerCodeSandbox.executeCode(executeCodeRequest);
+    }
 }
